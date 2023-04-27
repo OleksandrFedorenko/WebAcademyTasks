67278a1f86605954804fc6f816e7052a19c05500
Summer <afedorenko02@gmail.com>
2023-04-28 00:28:53 +0300

    4-5 tasks - done

--- Block1/Block1/ViewController.swift
+++ Block1/Block1/ViewController.swift
@@ -8,15 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        task1()
-//        task2()
-//        task3()
+        //task1()
+        //task2()
+        //task3()
+        //task4()
+        task5()
     }
-
-    /* Task 1 -   Создайте массив с десятью случайными числами, из которых пять меньше нуля и пять больше нуля.
+    
+    /* Task 1 - Создайте массив с десятью случайными числами, из которых пять меньше нуля и пять больше нуля.
      Замените все отрицательные числа на 0 и выведите массив в консоль*/
     func task1() {
         var arr: [Int] = [1, 2, 3, 4, 5, -1, -2, -3, -4, -5]
@@ -30,7 +32,7 @@ class ViewController: UIViewController {
         }
         print(arr)
     }
-
+    
     /* Task 2 -   Создайте массив с десятью случайными числами, из которых пять меньше нуля и пять больше нуля.
      Найдите минимальный и максимальный элемент в массиве и выведите его в консоль*/
     func task2() {
@@ -52,8 +54,25 @@ class ViewController: UIViewController {
     
     /* Task 4 - Создайте массив с названиями всех месяцев, типа String. Затем создайте словарь и с помощью цикла задайте ему
      значения на основании массива: где в качестве ключей будут выступать цифры (индексы), а в качестве значений - элементы массива.*/
+    //Check are there any different ways to solve this task
     func task4() {
-        
+        let arr: [String] = ["January", "February", "March", "April", "May", "June", "July", "August", "September","October", "November", "December"]
+        var dict: [Int:String] = [:]
+        for (k, v) in arr.enumerated(){
+            dict[k] = v
+        }
+        print(dict)
+    }
+    
+    /* Task 5 - Создайте словарь:
+     var dictionary = ["first": 1, "second": 2, "third": 3, "fourth": 4]
+     Поменяйте местами значения по ключам “first” и “fourth”. Выведите в консоль итоговый словарь*/
+    func task5() {
+        var dictionary = ["first": 1, "second": 2, "third": 3, "fourth": 4]
+        let first = dictionary.first?.value
+        dictionary["first"] = dictionary["fourth"]
+        dictionary["fourth"] = first
+        print(dictionary)
     }
 }
 
