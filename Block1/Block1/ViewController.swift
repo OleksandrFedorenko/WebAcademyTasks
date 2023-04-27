cceb588ef6fec04eece37afe5025158b01f22753
Summer <afedorenko02@gmail.com>
2023-04-28 01:14:43 +0300

    6-7 have been done, 8 is pending

--- Block1/Block1/ViewController.swift
+++ Block1/Block1/ViewController.swift
@@ -15,7 +15,9 @@ class ViewController: UIViewController {
         //task2()
         //task3()
         //task4()
-        task5()
+        //task5()
+        //task6_7()
+        task8()
     }
     
     /* Task 1 - Создайте массив с десятью случайными числами, из которых пять меньше нуля и пять больше нуля.
@@ -73,6 +75,39 @@ class ViewController: UIViewController {
         dictionary["first"] = dictionary["fourth"]
         dictionary["fourth"] = first
         print(dictionary)
+    }
+    
+    /* Task 6, 7 - Создайте пустой словарь calendar, так же вам понадобиться массив с месяцами из четвертого задания.
+     С помощью цикла от 1970 до 2022 задайте значения для словаря. В качестве ключа пусть будет год, а значение это массив с месяцами.
+     С помощью другого цикла добавьте в calendar на каждый год по одному месяцу с вашим названием, чтобы в каждом году вышло 13 месяцев.*/
+    
+    func task6_7() {
+        var calendar:[Int: [String]] = [:]
+        let arr: [String] = ["January", "February", "March", "April", "May", "June", "July", "August", "September","October", "November", "December"]
+        for i in 1970...2022{
+            calendar[i] = arr
+        }
+        for i in calendar{
+            calendar[i.key]?.append("YOBANA_rusNYA")
+        }
+        print(calendar)
+    }
+    
+    /* Task 8 - На основании словаря, который у вас получился в седьмом задании, давайте создадим новый словарь, который будет включать в себя также и даты, a именно массив с числами от 1 до 31:
+     ● Создайте массив с числами от 1 до 31 с помощью цикла.
+     ● Создайте новый словарь calendarPro, где в качестве ключей будут года, в качестве
+     значений вложенные словари. В которых в качестве ключей названия месяцев, а в
+     качестве значений массив с днями созданный выше.
+     ● Выведите в консоль 1 сентября 2019 года.
+     PS: Для заполнения calendarPro используйте только созданный массив с числами, и словарь с месяцами, и никаких других данных*/
+    func task8() {
+        var arr:[Int] = []
+        for i in 1...31{
+            var arr = arr.append(i)
+        }
+        var calendarPro:[Int:[String:[Int]]] = [:]
+        print(arr)
+        //next will be tomorrow
     }
 }
 
