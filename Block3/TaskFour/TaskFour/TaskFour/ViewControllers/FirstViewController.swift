a3f1a99021fed6edb5eb54f4f0c5e34cdd1fbbd1
Summer <afedorenko02@gmail.com>
2023-05-15 15:38:00 +0300

    Task 4 done

---
+++ Block3/TaskFour/TaskFour/TaskFour/ViewControllers/FirstViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FirstViewController.swift
+//  TaskFour
+//
+//  Created by Олександр Федоренко on 15.05.2023.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var bounceButton: UIButton!
+    @IBOutlet weak var ballView: UIView!
+    @IBOutlet weak var bottomBallViewConstraint: NSLayoutConstraint!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ballViewAdjust()
+    }
+    
+    func ballViewAdjust() {
+        ballView.layer.cornerRadius = 75
+    }
+
+    func animateBall() {
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       usingSpringWithDamping: 4.0,
+                       initialSpringVelocity: 5,
+                       options: [.autoreverse, .repeat]) {
+            self.bottomBallViewConstraint.constant = -400
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    @IBAction func bounceButtonPressed(_ sender: UIButton) {
+        animateBall()
+    }
+}
